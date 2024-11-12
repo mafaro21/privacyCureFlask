8812bb672ab7dc1df8e0730aa35d6ea6025e5097
mafaro21 <mafaro2105@gmail.com>
2024-11-12 16:48:46 +0200

    first

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, render_template,  redirect, url_for, send_from_directory
+import os
+from flask_mail import Mail, Message
+
+app = Flask(__name__, template_folder='')
+
+app.config['MAIL_SERVER'] = 'smtp.office365.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'ticket-alerts@providencehumancapital.com'
+# app.config['MAIL_RECIEVER'] = 'tickets@providencehumancapital.com' 
+# app.config['MAIL_RECIEVER'] = 'thexvi51@gmail.com' 
+app.config['MAIL_PASSWORD'] = 'Y@235813455857on'
+
+mail = Mail(app)
+
+# @app.route('/')
+# def index():
+#     return send_from_directory('C:/me/PrivacyCure/', 'index.html')
+    
+
+@app.route('/submit-form', methods=['POST'])
+def submit_form():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    request_type = request.form.get('request')
+    message = request.form.get('message')
+    
+    # For demonstration, let's print the data to the console
+    print(f'Name: {name}, Email: {email}, Phone: {phone}, Request: {request_type}  Message: {message}')
+    
+    # app.config['MAIL_SERVER'] = 'smtp.office365.com'
+    # app.config['MAIL_PORT'] = 587
+    # app.config['MAIL_USE_TLS'] = True
+    # app.config['MAIL_USE_SSL'] = False
+    # app.config['MAIL_USERNAME'] = 'ticket-alerts@providencehumancapital.com'
+    # app.config['MAIL_RECIEVER'] = 'tickets@providencehumancapital.com' 
+    # app.config['MAIL_RECIEVER'] = 'thexvi51@gmail.com' 
+    # app.config['MAIL_PASSWORD'] = 'Y@235813455857on'
+
+    # mail = Mail(app)
+
+    subject = 'New client message'
+    message_body = message
+
+    msg = Message(subject = subject,
+                  sender = app.config['MAIL_USERNAME'],
+                  recipients = ['dpo@privacycure.com'] ) # Send to this email address
+    msg.body = f'''Good day team,\n\nThere is a new message from a client: 
+    Name: {name}
+    Email: {email}
+    Phone: {phone}
+    Request: {request_type}
+    \n\n{message_body} 
+    \n\nRegards'''
+
+    try:
+        # mail.connect()
+        mail.send(msg)
+        return '<div style="text-align: center; margin-top:20vh; font-size:20px;">Thank you for you submission!, We will get back to you soon <div> To go home: <a href="https://privacycure.com">click here!!</a> </div></div>'
+        # return 'Email sent successfully!'
+    except Exception as e:
+        return f'Failed to send email: {e}'
+
+    # Redirect or show a success page after form submission
+    return '<div style="text-align: center; margin-top:20vh; font-size:20px;">Thank you for you submission!, We will get back to you soon <div> To go home: <a href="https://privacycure.com">click here!!</a> </div></div>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
